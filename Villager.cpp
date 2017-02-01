d19b6cd08b9deb6063bd6e94c572ad80c46ee973
xLeachimx <michael.huelsman@gmail.com>
2017-01-31 20:35:23 -0500

    villager fleshed out

--- Villager.cpp
+++ Villager.cpp
@@ -6,3 +6,42 @@
 */
 
 #include "Villager.hpp"
+
+Villager::Villager(){
+	alive = true;
+	resources = 0;
+}
+
+Villager::Villager(int starting){
+	alive = true;
+	resources = starting;
+}
+
+Villager::Villager(const Villager &copy){
+	alive = copy.alive;
+	resources = copy.resources;
+}
+
+//villager methods
+int Villager::worth(){
+	return resources;
+}
+
+bool Villager::isAlive(){
+	return alive;
+}
+
+void Villager::addResources(int num){
+	resources += num;
+}
+
+void Villager::kill(){
+	alive = false;
+}
+
+//operators
+Villager &Villager::operator=(const Villager &copy){
+	alive = copy.alive;
+	resources = copy.resources;
+	return *this;
+}
